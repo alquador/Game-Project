e06811fca83cf4a0199b965b8d4c18e9bcfe0011
alquador <97924027+alquador@users.noreply.github.com>
2022-02-23 07:57:12 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,12 +1,22 @@
 
 # Game-Project
 ## Matching Game
-[Basic Rendering](https://miro.com/app/board/uXjVOLxOiSw=/?invite_link_id=641943164282)
+[Basic Rendering]("/Users/quadorfamily/sei/projects/game-project/Images/My First Board (2).jpg")
 
-1. Build with grid or flex box
+[![First Rendering image]()]**IMAGE?
+
+### User Story
+The single user will be able to start clicking on the cards to reveal the image on the other side.  Toggling through as many times as needed to win the game.  A moves made counter will add 1 "Move made" for every two cards that are revealed.  The user will be able to see how many attempts it took to successful win and end the game.  There will also be a game timer, that stops once all cards are matched.
+
+
+### Build Steps
+
+1. Find images to access them in the game as the game card match potentials.
 2. Maybe try adding neat images to the cards...like a theme.
 3. Make the cards reactive to clicks
 4. Determine winning matches
 5. Try adding a timer
 6. Try tracking how many moves it takes to complete
-7. Play again...
+7. Play again button to reset game...
+8. Maybe try adding a completion song to play once all matches are made
+
---
+++ app.js
@@ -0,0 +1,123 @@
+//querySelector to be able to access game componenets   
+const gameCards = document.querySelectorAll('.game-card');
+const playAgain = document.querySelector('#play-again')
+const movesMade = document.querySelector('.moves-made')
+const gameTimer = document.querySelector('.timer')
+const wonderWoman0 = document.querySelector('#ww1')
+const wonderWoman1 = document.querySelector('#ww2')
+const thor2 = document.querySelector('#thor1')
+const thor3 = document.querySelector('#thor2')
+const iron4 = document.querySelector('#iron1')
+const iron5 = document.querySelector('#iron2')
+const hulk6 = document.querySelector('#hulk1')
+const hulk7 = document.querySelector('#hulk2')
+const captainAmerica8 = document.querySelector('#cap1')
+const captainAmerica9 = document.querySelector('#cap2')
+const blackWidow10 = document.querySelector('#bw1')
+const blackWidow11 = document.querySelector('#bw2')
+const blackPanther12 = document.querySelector('#bp1')
+const blackPanther13 = document.querySelector('#bp2')
+const avengers14 = document.querySelector('#avenge1')
+const avengers15 = document.querySelector('#avenge2')
+
+//make an array of the card choices...may help with shuffling them for each new game
+const cardChoices = [wonderWoman0, wonderWoman1, thor2, thor3, iron4, iron5, hulk6, hulk7,
+    captainAmerica8, captainAmerica9, blackWidow10, blackWidow11, blackPanther12, blackPanther13,
+    avengers14, avengers15]
+
+//set an empty array to push carch choices into, then check for match by index
+let playerChoice = ""
+console.log(playerChoice)
+//Randomize where the cards are on the game board each time the game is played
+const shuffleCards = () => {
+  gameCards.forEach(shuffleIndexOrder => {
+  let shuffle = Math.floor(Math.random() * 16)
+  shuffleIndexOrder.style.order = shuffle
+  })
+}
+shuffleCards()
+
+//I want the first click and second click to be appended into playerChoice array
+//use .push or append maybe...
+//I want to be able to do a match check within the indices of the array
+//after there is a match I want to remove the event listener or 'flip' to matched cards
+//to disable cards from being reflipped
+//if there is no match between the two cards they need to flip back
+//const checkForMatch = () => {
+  //let choice1 = ""
+  //let choice2 = ""
+//} if (playerChoice[0] === playerChoice1) {
+ // console.log("match")
+ // )
+//}
+
+
+
+const clearGame = () => { 
+  window.location.reload()
+}
+
+//Attach a click counter to account for how many "Moves Made" will populate
+let clicks = 0
+let moves = 0
+movesMade.innerHTML = "Moves made: " + moves
+const clickCounter = () => {
+  clicks++
+  console.log(clicks)
+}
+
+//Attach a start and stop timer to clock how long it takes to sucessfully complete the game
+//there are 1,000 milliseconds in 1 second
+let seconds = 0
+let minutes = 0
+let time = ""
+let timeStart = false
+const startTimer = () => {
+  time = setInterval(function() {
+    seconds++
+    if(seconds === 60) {
+      minutes++
+      seconds = 0
+    }
+    gameTimer.innerHTML = "Timer: " + minutes + " minutes " + seconds + " seconds "
+  }, 1000)
+}
+startTimer()
+
+//Need to invoke stop timer once all matches are made...
+const stopTimer = () => {
+  clearInterval(time)
+}
+//create a function that stops time and moves on the game board once the game is won
+//if (matched.length === 16)
+const gameWon = () => { 
+  //invoke the stopTimer function to stop the time
+  //stopTimer()
+  //reset moves to 0
+  //moves = 0 
+}
+//Flip card to reveal the character image
+function flipCard(event) {
+  this.classList.toggle('flip')
+  clickCounter()
+  console.log(event.target.id)
+  if (clicks === 2) {
+    clicks.appendChild(playerChoice)
+    //checkForMatch()
+    moves++
+    movesMade.innerHTML = "Moves made: " + moves
+    clicks = 0
+  }
+}
+
+gameCards.forEach(card => card.addEventListener('click', flipCard))
+
+document.addEventListener('DOMContentLoaded', () => {
+  playAgain.addEventListener('click', clearGame)
+  movesMade.addEventListener('click', clickCounter)
+})
+
+//If I can complete everything I want... maybe add a completion song
+
+
+
